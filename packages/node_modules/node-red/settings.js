2797ad4bb9746b0cbc1302d3422b98118d6c3c90
yohanswanepoel <johannes.swanepoel@gmail.com>
2019-07-09 09:16:31 +1000

    Update settings.js

--- packages/node_modules/node-red/settings.js
+++ packages/node_modules/node-red/settings.js
@@ -20,7 +20,7 @@
 
 module.exports = {
     // the tcp port that the Node-RED web server is listening on
-    uiPort: process.env.PORT || 1880,
+    uiPort: process.env.PORT || 8080,
 
     // By default, the Node-RED UI accepts connections on all IPv4 interfaces.
     // To listen on all IPv6 addresses, set uiHost to "::",
